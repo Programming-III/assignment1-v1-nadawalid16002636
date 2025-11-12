2a0e96b8294823bec4582181eafea4ea0d5f507a
nadawalid16002636 <nadaabouamer5@gmail.com>
2025-11-12 15:08:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,15 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
+    int main() {
+    addAnimal(Animal* a);
+    std::cout<< "Enclosure" << addAnimal << endl;
     
-    
+    displayMammal("Lion", 5 , Hungry);
+    displayBird("Parrot", 2 , Not Hungry);
+    displayReptile("Snake", 3 , Venomous, Hungry);
+
+    displayinfo( "Sarah Ali" , 3);
+
     return 0;
 }
