a524ad4aef4f872ad089f2c77688c72dcb61c931
nadawalid16002636 <nadaabouamer5@gmail.com>
2025-11-12 15:03:23 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -4,7 +4,25 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal() {
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    
+    public:
+    void display(string name,int age,bool isHungry)
+      this->name=name;
+      this->age=age;
+      this->isHungry=isHungry;
+      
+      std::cout << name << endl;
+      std::cout << "Age:" << age << endl;
+      std::cout << isHungry << endL;
+      
+      void feed(bool isHungry)
+      this->isHungry=isHungry;
+}
 
 
 
